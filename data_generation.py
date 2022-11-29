64b8d0c2b5d2fe57928025e244a8e237eb254651
David Drobner <david@drobner.ca>
2022-11-29 00:12:34 -0500

    Data generation all good (for now)

--- data_generation.py
+++ data_generation.py
@@ -6,29 +6,44 @@
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# we'll have a dataset of size approx. 5000 elements
+# we'll have a dataset of size approx. 2000 elements
 # should be enough for one parameter
-length = np.arange(0.01, 50.0, 0.01)
+length = np.arange(0.1, 100, 0.1)
 g = 9.81 # gravity...
 
-def dtheta(y, t, length_g):
+# define our function which returns dy/dt
+def dy(y, t, length_g):
     theta, omega = y
     dy = [omega, -length_g*np.sin(theta)]
     return dy
 
-t = np.linspace(0, 20, 1000)
+# set up our sample space
+t = np.linspace(0, 60, 4000)
 y_0 = [np.pi-0.1, 0]
-sol = odeint(dtheta, y_0, t, args=(10,))
+"""
+#this block was testing the method I was using
 
-#plt.plot(t, sol[:, 0])
-#plt.show()
+sol = odeint(dy, y_0, t, args=(0.23513,))
 
+# perform a fft to find the period
 ft = np.fft.rfft(sol[:, 0])
 freqs = np.fft.rfftfreq(len(sol[:, 0]), t[1]-t[0])
 mags = abs(ft)
+"""
+# ok... we can do it for a single omega
+# let's generate a dataset now
+periods = np.empty(len(length))
 
-print(1/freqs[mags.argmax()])
+k = 0
+for l in length:
+    sol_l = odeint(dy, y_0, t, args=((g/l),))
+    fft = np.fft.rfft(sol_l[:, 0])
+    freq = np.fft.rfftfreq(len(sol_l[:, 0]), t[1]-t[0])
+    mag = abs(fft)
+    periods[k] = 1/freq[mag.argmax()]
+    k += 1
 
-plt.plot(t, sol[:, 0])
-plt.show()
+out = pd.DataFrame({"Length" : length, "Period": periods})
+out.to_csv("shm_data.csv", index=False)
