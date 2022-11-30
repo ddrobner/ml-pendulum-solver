c6423de1b0b061ef298dcb35b1f059954763fdca
David Drobner <david@drobner.ca>
2022-11-29 23:26:57 -0500

    Change from fft to autocorrelation

--- data_generation.py
+++ data_generation.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 # we'll have a dataset of size approx. 2000 elements
 # should be enough for one parameter
-length = np.arange(0.1, 100, 0.1)
+length = np.arange(1, 20, 0.1)
 g = 9.81 # gravity...
 
 # define our function which returns dy/dt
@@ -20,17 +20,31 @@ def dy(y, t, length_g):
     return dy
 
 # set up our sample space
-t = np.linspace(0, 60, 8000)
+sample_rate = 0.01
+t_max = 60
+t = np.linspace(0, t_max, int(t_max/sample_rate))
 y_0 = [np.pi-0.1, 0]
-"""
+
 #this block was testing the method I was using
+sol = odeint(dy, y_0, t, args=((g/1),))
 
-sol = odeint(dy, y_0, t, args=(0.23513,))
-
+"""
 # perform a fft to find the period
 ft = np.fft.rfft(sol[:, 0])
 freqs = np.fft.rfftfreq(len(sol[:, 0]), t[1]-t[0])
 mags = abs(ft)
+
+theta_t = sol[:, 0]
+
+# using autocorrelation to find the period of the solution
+acf = np.correlate(theta_t, theta_t, 'full')[-len(theta_t):]
+
+inflection = np.diff(np.sign(np.diff(acf)))
+peaks = (inflection < 0).nonzero()[0] + 1
+delay = peaks[acf[peaks].argmax()]
+print(t[delay]-t[0])
+
+
 """
 # ok... we can do it for a single omega
 # let's generate a dataset now
@@ -38,12 +52,15 @@ periods = np.empty(len(length))
 
 k = 0
 for l in length:
-    sol_l = odeint(dy, y_0, t, args=((g/l),), hmax=0.001)
-    fft = np.fft.rfft(sol_l[:, 0])
-    freq = np.fft.rfftfreq(len(sol_l[:, 0]), t[1]-t[0])
-    mag = abs(fft)
-    periods[k] = 1/freq[mag.argmax()]
+    # solve ode
+    sol_l = odeint(dy, y_0, t, args=((g/l),))
+    theta_t = sol_l[:, 0]
+    # autocorrelate
+    acf = np.correlate(theta_t, theta_t, 'full')[-len(theta_t):]
+    inflection = np.diff(np.sign(np.diff(acf)))
+    peaks = (inflection < 0).nonzero()[0] + 1
+    delay = peaks[acf[peaks].argmax()]
+    periods[k] = (t[delay]-t[0])
     k += 1
-
 out = pd.DataFrame({"Length" : length, "Period": periods})
 out.to_csv("shm_data.csv", index=False)
